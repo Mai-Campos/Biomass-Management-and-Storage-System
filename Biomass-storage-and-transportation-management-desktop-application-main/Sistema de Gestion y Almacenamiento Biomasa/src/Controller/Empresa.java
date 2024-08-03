89ca80c0a476bd4ba17ee2f422e8eeef6b6d6f22
Maikol <Camposmaikol1@gmail.com>
2024-08-03 18:24:17 -0400

    Se modifica la clase Empresa

--- Biomass-storage-and-transportation-management-desktop-application-main/Sistema de Gestion y Almacenamiento Biomasa/src/Controller/Empresa.java
+++ Biomass-storage-and-transportation-management-desktop-application-main/Sistema de Gestion y Almacenamiento Biomasa/src/Controller/Empresa.java
@@ -1,5 +1,47 @@
 package Controller;
 
+import java.util.ArrayList;
+
+import Model.Almacen;
+import Model.Empleado;
+import Model.Vehiculo;
+
 public class Empresa {
 
+    protected ArrayList<Almacen> almacenesList;
+    protected ArrayList<Empleado> empleadosList;
+    protected ArrayList<Vehiculo> vehiculosList;
+
+    public Empresa() {
+        almacenesList = new ArrayList<>();
+        empleadosList = new ArrayList<>();
+        vehiculosList = new ArrayList<>();
+    }
+
+    
+    public void agregarAlmacen(Almacen almacen) {
+        almacenesList.add(almacen);
+    }
+
+    public void eliminarAlmacen(Almacen almacen) {
+        almacenesList.remove(almacen);
+    }
+
+    public void agregarEmpleado(Empleado empleado) {
+        empleadosList.add(empleado);
+    }
+
+    public void eliminarEmpleado(Empleado empleado) {
+        empleadosList.remove(empleado);
+    }
+
+    public void agregarVehiculo(Vehiculo vehiculo) {
+        vehiculosList.add(vehiculo);
+    }
+
+    public void eliminarVehiculo(Vehiculo vehiculo) {
+        vehiculosList.remove(vehiculo);
+    
+    
+}
 }
